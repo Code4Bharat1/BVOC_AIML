9097222f0e48a884fcd92af2b0afc7018592414a
Shams Ali <shaikhshams59@gmail.com>
2025-04-19 18:13:42 +0530

    final

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,7 +1,7 @@
 import Navbar from "@/components/Navbar";
 import CareerOpportunitiesTable from "@/components/CareerOpp";
 import HeroAbout from "@/components/HeroAbout";
-
+import SemesterCards from "@/components/Syllabuscards/SemesterCards";
 const Home = () => {
   return (
     <div className="bg-[#fafaf3] pt-24">

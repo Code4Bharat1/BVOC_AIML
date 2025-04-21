5baa9fcae596b612834220718c7704bb45aef0ed
Ahmed Solanki <ahmedsolanki.dev@gmail.com>
2025-04-21 18:13:21 +0530

    changes

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,6 +2,7 @@ import Navbar from "@/components/Navbar";
 import CareerOpportunitiesTable from "@/components/CareerOpp";
 import HeroAbout from "@/components/HeroAbout";
 import SemesterCards from "@/components/Syllabuscards/SemesterCards";
+
 const Home = () => {
   return (
     <div className="bg-[#fafaf3] pt-24">

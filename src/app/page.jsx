f4e22b029bc2579c93eefe53c1d6f401e829fedb
noumankhan20 <noumankhan8097@gmail.com>
2025-04-23 15:23:54 +0530

    Program Structure Component and image

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,12 +3,15 @@ import CareerOpportunitiesTable from "@/components/CareerOpp";
 import HeroAbout from "@/components/HeroAbout";
 import SemesterCards from "@/components/Syllabuscards/SemesterCards";
 import Footer from "@/components/Footer";
+import Programs from "@/components/ourProgram/progarm";
+
 const Home = () => {
   return (
     <div className="bg-[#fafaf3] pt-24">
       <Navbar />
       <HeroAbout />
       <CareerOpportunitiesTable />
+      <Programs/>
       <SemesterCards />
       <Footer />
     </div>

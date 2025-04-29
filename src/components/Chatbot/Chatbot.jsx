72fd0b6c63ba0913684c734c8483bb27e7fe64c3
noumankhan20 <noumankhan8097@gmail.com>
2025-04-29 16:06:45 +0530

    final

--- src/components/Chatbot/Chatbot.jsx
+++ src/components/Chatbot/Chatbot.jsx
@@ -14,7 +14,7 @@ export default function ChatbotWidget() {
 
   const fetchBotResponse = async (userMessage) => {
     try {
-      const response = await fetch('http://193.203.260.238:5869/chat', {
+      const response = await fetch('http://193.203.160.238:5869/chat', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ message: userMessage }),

80f7a4170a710a20f1d1fbefc580f045c7d6d5e9
yogu-code <yogeshrane019@gmail.com>
2025-05-15 17:31:34 +0530

    Add AOS library for animations and refactor AllSemesters component layout

--- src/components/Learn&Grow/AllSemesters.jsx
+++ src/components/Learn&Grow/AllSemesters.jsx
@@ -1,97 +1,180 @@
-import { useEffect, useState } from 'react';
+"use client";
 
-const SemesterCard = ({ position, number, title, isVisible }) => {
-  const isLeft = position === 'left';
-  const svgPath = isLeft ? '/elements/SemesterLeft.svg' : '/elements/SemesterRight.svg';
-  
-  return (
-    <div 
-      className={`w-full flex ${isLeft ? 'justify-start' : 'justify-end'} mb-6 transition-all duration-700 transform ${isVisible ? 'translate-x-0 opacity-100' : (isLeft ? '-translate-x-full' : 'translate-x-full') + ' opacity-0'}`}
-    >
-      <div className="w-5/6 relative">
-        {/* SVG background from external file */}
-        <div className="relative">
-          <img 
-            src={svgPath} 
-            alt={`Semester ${number} background`} 
-            className="w-full h-auto"
-          />
-          
-          {/* Content overlaid on the SVG */}
-          <div className="absolute inset-0 flex flex-col justify-center px-12">
-            <h3 className="text-white font-bold text-lg mb-1">SEMESTER {number}</h3>
-            <p className="text-white text-sm">{title}</p>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
-};
+import Image from "next/image";
+import AOS from "aos";
+import "aos/dist/aos.css";
+import { useEffect } from "react";
 
 const AllSemesters = () => {
-  const [visibleItems, setVisibleItems] = useState([]);
-  
-  // Semester data
-  const semesters = [
-    {
-      position: 'left',
-      number: 1,
-      title: 'YOU WILL LEARN HOW TO CREATE FAST AND EFFICIENT APPROACHES FOR LARGE SYSTEMS'
-    },
-    {
-      position: 'right',
-      number: 2,
-      title: 'FUNDAMENTAL KNOWLEDGE OF DIGITAL SECURITY FOR BEGINNERS'
-    },
-    {
-      position: 'left',
-      number: 3,
-      title: 'A DEEP DIVE INTO PROTECTING COMPLEX SYSTEMS AND ARCHITECTURES'
-    },
-    {
-      position: 'right',
-      number: 4,
-      title: 'YOU WILL LEARN LEVEL: MIDDLE LEVEL'
-    },
-    {
-      position: 'left',
-      number: 5,
-      title: 'YOU WILL LEARN LEVEL: SENIOR LEVEL'
-    },
-    {
-      position: 'right',
-      number: 6,
-      title: 'YOU WILL LEARN LEVEL: ADVANCED LEVEL'
-    }
-  ];
-
   useEffect(() => {
-    // Animate cards sequentially
-    const animationDelay = 300; // ms between each card animation
-    
-    semesters.forEach((_, index) => {
-      setTimeout(() => {
-        setVisibleItems(prev => [...prev, index]);
-      }, index * animationDelay);
+    AOS.init({
+      duration: 800,
+      once: true, // 👈 Only animate on scroll-in, no reverse on scroll-out
     });
   }, []);
 
   return (
-    <div className="lg:hidden py-6 px-4 bg-gray-900">
-      <div className="text-center mb-6">
-        <h2 className="text-purple-400 font-bold text-xl bg-purple-200 bg-opacity-20 py-2 px-4 rounded-full inline-block">ALL SEMESTERS</h2>
-      </div>
-      
-      <div className="flex flex-col">
-        {semesters.map((semester, index) => (
-          <SemesterCard 
-            key={index}
-            position={semester.position}
-            number={semester.number}
-            title={semester.title}
-            isVisible={visibleItems.includes(index)}
+    <div className="block md:hidden">
+      <div className="relative w-full max-w-6xl mx-auto h-[1400px] px-4 py-8 bg-gray-900">
+        {/* Heading */}
+        <div className="absolute top-10 left-1/2 transform -translate-x-1/2">
+          <div className="bg-[#D8BCFD] border-2 border-white rounded-2xl p-6 text-[#903BFF] w-fit font-extrabold">
+            <p>All Semesters</p>
+          </div>
+        </div>
+        {/* Semester 1 */}
+        <div
+          data-aos="fade-right"
+          className="absolute top-[160px] left-1/5 text-2xl text-white"
+        >
+          <h1>Semester 1</h1>
+        </div>
+        <div data-aos="fade-right" className="absolute top-[200px] left-4">
+          <Image
+            src="/elements/SemesterLeft.svg"
+            alt="Left Top"
+            width={250}
+            height={200}
           />
-        ))}
+        </div>
+        <div
+          data-aos="fade-right"
+          className="absolute top-[210px] left-18 text-white"
+        >
+          <p>
+            You will learn how to <br />
+            create user-friendly <br />
+            and efficient interfaces <br />
+            for large System
+          </p>
+        </div>
+        {/* Semester 2 */}
+        <div
+          data-aos="fade-left"
+          className="absolute top-[360px] right-1/5 text-2xl text-white"
+        >
+          <h1>Semester 2</h1>
+        </div>
+        <div data-aos="fade-left" className="absolute top-[400px] right-4">
+          <Image
+            src="/elements/SemesterRight.svg"
+            alt="Right Mid"
+            width={250}
+            height={200}
+          />
+        </div>
+        <div
+          data-aos="fade-left"
+          className="absolute top-[410px] right-20 text-white"
+        >
+          <p>
+            Fundamental <br />
+            knowledge <br />
+            of digital security <br />
+            for beginners
+          </p>
+        </div>
+        {/* Semester 3 */}
+        <div
+          data-aos="fade-right"
+          className="absolute top-[560px] left-1/5 text-2xl text-white"
+        >
+          <h1>Semester 3</h1>
+        </div>
+        <div data-aos="fade-right" className="absolute top-[600px] left-4">
+          <Image
+            src="/elements/SemesterLeft.svg"
+            alt="Left Lower"
+            width={250}
+            height={200}
+          />
+        </div>
+        <div
+          data-aos="fade-right"
+          className="absolute top-[610px] left-20 text-white"
+        >
+          <p>
+            A deep dive into <br />
+            protecting complex <br />
+            systems and <br />
+            architectures
+          </p>
+        </div>
+        {/* Semester 4 */}
+        <div
+          data-aos="fade-left"
+          className="absolute top-[760px] right-1/5 text-2xl text-white"
+        >
+          <h1>Semester 4</h1>
+        </div>
+        <div data-aos="fade-left" className="absolute top-[800px] right-4">
+          <Image
+            src="/elements/SemesterRight.svg"
+            alt="Right Lower"
+            width={250}
+            height={200}
+          />
+        </div>
+        <div
+          data-aos="fade-left"
+          className="absolute top-[825px] right-20 text-white"
+        >
+          <p>
+            You will Learn <br />
+            Level: Middle <br />
+            level
+          </p>
+        </div>
+        {/* Semester 5 */}
+        <div
+          data-aos="fade-right"
+          className="absolute top-[960px] left-1/5 text-2xl text-white"
+        >
+          <h1>Semester 5</h1>
+        </div>
+        <div data-aos="fade-right" className="absolute top-[1000px] left-4">
+          <Image
+            src="/elements/SemesterLeft.svg"
+            alt="Left Lower"
+            width={250}
+            height={200}
+          />
+        </div>
+        <div
+          data-aos="fade-right"
+          className="absolute top-[1025px] left-20 text-white"
+        >
+          <p>
+            You will Learn <br />
+            Level: beginner <br />
+            level
+          </p>
+        </div>
+        {/* Semester 6 */}
+        <div
+          data-aos="fade-left"
+          className="absolute top-[1160px] right-1/5 text-2xl text-white"
+        >
+          <h1>Semester 6</h1>
+        </div>
+        <div data-aos="fade-left" className="absolute top-[1200px] right-4">
+          <Image
+            src="/elements/SemesterRight.svg"
+            alt="Right Lower"
+            width={250}
+            height={200}
+          />
+        </div>
+        <div
+          data-aos="fade-left"
+          className="absolute top-[1220px] right-20 text-white"
+        >
+          <p>
+            You will Learn <br />
+            Level: Advanced <br />
+            level
+          </p>
+        </div>
       </div>
     </div>
   );

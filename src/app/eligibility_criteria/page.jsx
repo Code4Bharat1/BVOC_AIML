4a397fd6f0ec7549d8e69b5976cc9fd6acaf460e
Resource-07 <resource7.code4bharat@gmail.com>
2025-05-17 14:10:12 +0530

    Add Admission Eligibility component and update import path

--- src/app/eligibility_criteria/page.jsx
+++ src/app/eligibility_criteria/page.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Navbar from "@/components/layout/Navbar";
 import Footer from "@/components/layout/Footer";
-import Admission_Eligibility from "@/components/Eligibility_Criteria/Admission_Eligibility";
+import Admission_Eligibility from "@/components/Opportunities/Admission_Eligibility";
 export default function page() {
   return (
     <div className="flex flex-col min-h-screen ">
